78294012b2092aa647895d352eda8f80b26d419b
True1Santony <espartaconassekine@gmail.com>
2023-12-13 16:12:35 +0100

    Ajustes en la tarea

--- Tema6/app/src/main/java/nassekine/spartak/tarea06/MainActivity.java
+++ Tema6/app/src/main/java/nassekine/spartak/tarea06/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
@@ -17,17 +18,20 @@ import com.google.android.exoplayer2.ui.StyledPlayerView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button reproducir, reprodLocalServ;
     ExoPlayer player;
     String urlVideo = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
-    String urlVideoRemoro="https://MiNas-221.eu.tnas.link/tos/#/share?share_link=CAIYCNKxi%2Bz4jdD0%2B9tnpc19H%2BL2mUkL332oOjRk%2F1QFtiA%3D";
+    String urlVideoLocServ="http://192.168.1.8/tortugas.mp4";//no reproduce .avi
+    String elecion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        reproducir =findViewById(R.id.btnReproducir);
+        reprodLocalServ=findViewById(R.id.btpReprServLoc);
 
         //oculta la barra de arriba
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -40,16 +44,24 @@ public class MainActivity extends AppCompatActivity {
         StyledPlayerView playerView = findViewById(R.id.playerView);
         player = new ExoPlayer.Builder(MainActivity.this).build();
         playerView.setPlayer(player);
-        MediaItem mediaItem = MediaItem.fromUri(urlVideoRemoro);
-        player.setMediaItem(mediaItem);
-        player.prepare();
+
         //player.setPlayWhenReady(true);
     }
 
 
     public void botonPlay(View view) {
 
+       /* Button pulsado=new Button(this);
+        pulsado=(Button) view;*/
+        if(view.getId()==reproducir.getId()){
+          elecion=urlVideo;
+        }else{
+            elecion=urlVideoLocServ;
+        }
 
+        MediaItem mediaItem = MediaItem.fromUri(elecion);
+        player.setMediaItem(mediaItem);
+        player.prepare();
         player.setPlayWhenReady(true);
     }
 
@@ -60,4 +72,43 @@ public class MainActivity extends AppCompatActivity {
         return true;
 
     }
+   /* @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (player != null) {
+            outState.putLong("player_position", player.getCurrentPosition());
+            outState.putBoolean("player_play_when_ready", player.getPlayWhenReady());
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (player != null) {
+            long playerPosition = savedInstanceState.getLong("player_position", 0);
+            boolean playWhenReady = savedInstanceState.getBoolean("player_play_when_ready", true);
+            player.seekTo(playerPosition);
+            player.setPlayWhenReady(playWhenReady);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        player.release();
+    }*/
+   /* @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        //  orientación de la pantalla
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // modo paisaje
+            setContentView(R.layout.activity_main);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // modo retrato
+            setContentView(R.layout.activity_main_land);
+        }
+    }*/
 }
+
